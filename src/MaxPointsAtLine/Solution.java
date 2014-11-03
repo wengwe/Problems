e9c6d19ec7c910b2fcf4883d39c3dc7dc5b07e58
weng <wengwei.jason@gmail.com>
2014-11-03 23:58:39 +0800

    formatting.

--- src/MaxPointsAtLine/Solution.java
+++ src/MaxPointsAtLine/Solution.java
@@ -9,15 +9,23 @@ import static java.lang.Math.abs;
  */
 
 
-  class Point {
-      int x;
-      int y;
-      Point() { x = 0; y = 0; }
-      Point(int a, int b) { x = a; y = b; }
-  }
+class Point {
+    int x;
+    int y;
+
+    Point() {
+        x = 0;
+        y = 0;
+    }
+
+    Point(int a, int b) {
+        x = a;
+        y = b;
+    }
+}
 
 
-class Line{
+class Line {
     private final double a;
     private final double b;
     private final boolean isVertical;
@@ -29,11 +37,11 @@ class Line{
         this.isVertical = isVertical;
     }
 
-    public boolean pointOnThisLine(final PointForHashMap p){
-        if(isVertical) return p.x == a;
+    public boolean pointOnThisLine(final PointForHashMap p) {
+        if (isVertical) return p.x == a;
         else {
-            double r = a * p.x + b  - p.y;
-            return  (abs(r)  < PRECISION);
+            double r = a * p.x + b - p.y;
+            return (abs(r) < PRECISION);
         }
     }
 }
@@ -41,7 +49,11 @@ class Line{
 class PointForHashMap {
     final int x;
     final int y;
-    PointForHashMap(int a, int b) { x = a; y = b; }
+
+    PointForHashMap(int a, int b) {
+        x = a;
+        y = b;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -67,16 +79,16 @@ class PointForHashMap {
 public class Solution {
     public int maxPoints(Point[] points) {
         int numberOfPoints = points.length;
-        if(numberOfPoints == 0) return 0;
-        if(numberOfPoints == 1) return 1;
+        if (numberOfPoints == 0) return 0;
+        if (numberOfPoints == 1) return 1;
 
         Map<PointForHashMap, Integer> pointMap = new HashMap<PointForHashMap, Integer>();
-        for(Point p: points){
+        for (Point p : points) {
             PointForHashMap pointForHashMap = new PointForHashMap(p.x, p.y);
-            if(pointMap.containsKey(pointForHashMap)){
+            if (pointMap.containsKey(pointForHashMap)) {
                 int count = pointMap.get(pointForHashMap) + 1;
                 pointMap.put(pointForHashMap, count);
-            }else{
+            } else {
                 pointMap.put(pointForHashMap, 1);
             }
         }
@@ -84,58 +96,52 @@ public class Solution {
 
         List<PointForHashMap> uniquePoints = new ArrayList(pointMap.keySet());
         int uniquePointsNumber = uniquePoints.size();
-        if(uniquePointsNumber == 1) return   pointMap.get(uniquePoints.get(0));
+        if (uniquePointsNumber == 1) return pointMap.get(uniquePoints.get(0));
 
-        Map<Line, Integer> lineMap = new HashMap<Line,Integer>();
-        PointForHashMap p1 =  uniquePoints.get(0);
-        PointForHashMap p2 =  uniquePoints.get(1);
+        Map<Line, Integer> lineMap = new HashMap<Line, Integer>();
+        PointForHashMap p1 = uniquePoints.get(0);
+        PointForHashMap p2 = uniquePoints.get(1);
         Line line1 = resolveLineRepresentation(p1, p2);
-        if(line1 != null) lineMap.put(line1, pointMap.get(p1)+pointMap.get(p2));
+        if (line1 != null) lineMap.put(line1, pointMap.get(p1) + pointMap.get(p2));
 
-
-
-        for(int i=2; i < uniquePointsNumber ;i++){
+        for (int i = 2; i < uniquePointsNumber; i++) {
             PointForHashMap p = uniquePoints.get(i);
             boolean pointOnExistingLine = false;
-            for(Line line: lineMap.keySet()){
-                if(line.pointOnThisLine(p))   {
+            for (Line line : lineMap.keySet()) {
+                if (line.pointOnThisLine(p)) {
                     int count = lineMap.get(line);
                     count += pointMap.get(p);
                     lineMap.put(line, count);
                     pointOnExistingLine = true;
-
                 }
             }
             //if not on previous line. then each point pair should form a new line.
-            if(!pointOnExistingLine){
-                for(int j=0; j<i; j++){
+            if (!pointOnExistingLine) {
+                for (int j = 0; j < i; j++) {
                     PointForHashMap pToCheck = uniquePoints.get(j);
                     Line line = resolveLineRepresentation(pToCheck, p);
-                    if(line != null) lineMap.put(line, pointMap.get(pToCheck)+pointMap.get(p));
+                    if (line != null) lineMap.put(line, pointMap.get(pToCheck) + pointMap.get(p));
                 }
 
             }
         }
-
-
         return Collections.max(lineMap.values());
     }
 
 
+    private Line resolveLineRepresentation(PointForHashMap p1, PointForHashMap p2) {
 
-    private Line resolveLineRepresentation(PointForHashMap p1, PointForHashMap p2){
-
-        if(p1.equals(p2)) return null;
+        if (p1.equals(p2)) return null;
 
         double xDistance = p2.x - p1.x;
         double yDistance = p2.y - p1.y;
-        if(xDistance == 0)   {
-            return new Line(p1.x,0, true);
+        if (xDistance == 0) {
+            return new Line(p1.x, 0, true);
         }
 
         double a = yDistance / xDistance;
-        double b = p2.y - p2.x* a;
-        return new Line(a,b, false);
+        double b = p2.y - p2.x * a;
+        return new Line(a, b, false);
 
     }
 }
